af2e9dc1030689db83d08c76d41670fedcb52ca6
Dave <dave@localhost.localdomain>
2025-06-11 12:15:10 +0100

    fixed player page bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { browser } from '$app/environment';
   import { onMount, onDestroy } from 'svelte';
   import { supabase } from '$lib/supabaseClient';
   import VideoGrid from '$lib/VideoGrid.svelte';
