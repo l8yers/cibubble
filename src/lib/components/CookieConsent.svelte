2fd939d7addeba08cb2ba0075123acc49fb43868
Dave <dave@localhost.localdomain>
2025-06-27 11:36:33 +0100

    move to bottom

--- src/lib/components/CookieConsent.svelte
+++ src/lib/components/CookieConsent.svelte
@@ -19,27 +19,28 @@
 </script>
 
 {#if !accepted}
-  <div id="cookie-banner-test" style="
-    position: fixed;
-    left: 20vw;
-    top: 20vh;
-    width: 60vw;
-    height: 30vh;
-    background: #e93c2f;
-    color: #fff;
-    z-index: 1000000;
-    font-size: 2em;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 5px solid yellow;
-    box-shadow: 0 0 30px #000;
-    text-align: center;
-    ">
-    <div>
-      🚨 BANNER TEST: If you see this, the CookieConsent component is rendering! 🚨
-    </div>
-    <button on:click={acceptCookies} style="font-size:1em;padding:1em 2em;margin-top:2em;">OK</button>
+<div id="cookie-banner-test" style="
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 90px;
+  background: #e93c2f;
+  color: #fff;
+  z-index: 1000000;
+  font-size: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top: 5px solid yellow;
+  box-shadow: 0 0 30px #000;
+  text-align: center;
+  ">
+  <div>
+    🚨 BANNER TEST: Bottom Banner! 🚨
   </div>
+  <button on:click={acceptCookies} style="font-size:1em;padding:1em 2em;margin-top:2em;">OK</button>
+</div>
 {/if}
