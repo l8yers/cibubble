abf78fc66fb0a52ab82767b1c348105dbb75b980
Dave <dave@localhost.localdomain>
2025-06-27 11:14:45 +0100

    failsafe cookie banenr for testing

--- src/lib/components/CookieConsent.svelte
+++ src/lib/components/CookieConsent.svelte
@@ -1,61 +1,45 @@
 <script>
   import { onMount } from 'svelte';
   let accepted = false;
+
   onMount(() => {
     if (typeof window !== 'undefined') {
       accepted = window.localStorage.getItem('cookieConsent') === 'yes';
+      console.log('CookieConsent loaded, accepted:', accepted);
     }
   });
+
   function acceptCookies() {
     if (typeof window !== 'undefined') {
       window.localStorage.setItem('cookieConsent', 'yes');
       accepted = true;
+      console.log('Accepted set, banner should vanish');
     }
   }
 </script>
-<slot />
+
 {#if !accepted}
-  <div class="cookie-banner">
-    <span>
-      This is a test cookie banner.
-      <a href="/privacy" target="_blank" rel="noopener">Learn more</a>.
-    </span>
-    <button on:click={acceptCookies}>OK</button>
+  <div id="cookie-banner-test" style="
+    position: fixed;
+    left: 20vw;
+    top: 20vh;
+    width: 60vw;
+    height: 30vh;
+    background: #e93c2f;
+    color: #fff;
+    z-index: 1000000;
+    font-size: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 5px solid yellow;
+    box-shadow: 0 0 30px #000;
+    text-align: center;
+    ">
+    <div>
+      🚨 BANNER TEST: If you see this, the CookieConsent component is rendering! 🚨
+    </div>
+    <button on:click={acceptCookies} style="font-size:1em;padding:1em 2em;margin-top:2em;">OK</button>
   </div>
 {/if}
-
-<style>
-.cookie-banner {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: #222 !important;
-  color: #fff;
-  padding: 1.5em 2em;
-  z-index: 99999 !important;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 1.2em;
-  box-shadow: 0 -2px 12px #0006;
-  border-top: 3px solid #e93c2f;
-}
-.cookie-banner button {
-  background: #e93c2f;
-  color: #fff;
-  border: none;
-  padding: 0.7em 2em;
-  border-radius: 9px;
-  font-size: 1.1em;
-  font-weight: 700;
-  cursor: pointer;
-  margin-left: 2em;
-}
-.cookie-banner a {
-  color: #e93c2f;
-  text-decoration: underline;
-  margin-left: 0.7em;
-  font-size: 1em;
-}
-</style>
