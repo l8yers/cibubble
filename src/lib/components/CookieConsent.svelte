894955e2f41895e23d191312255fc41448f873af
Dave <dave@localhost.localdomain>
2025-06-27 11:32:27 +0100

    revert to working test banner

--- src/lib/components/CookieConsent.svelte
+++ src/lib/components/CookieConsent.svelte
@@ -1,118 +1,45 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount } from 'svelte';
   let accepted = false;
-  let el;
 
   onMount(() => {
     if (typeof window !== 'undefined') {
       accepted = window.localStorage.getItem('cookieConsent') === 'yes';
-      // Dynamically move this element to <body>
-      el = document.createElement('div');
-      el.id = "cookie-consent-root";
-      document.body.appendChild(el);
-      updateBanner();
+      console.log('CookieConsent loaded, accepted:', accepted);
     }
-  });
-
-  onDestroy(() => {
-    if (el && el.parentNode) el.parentNode.removeChild(el);
   });
 
   function acceptCookies() {
     if (typeof window !== 'undefined') {
       window.localStorage.setItem('cookieConsent', 'yes');
       accepted = true;
-      updateBanner();
-    }
-  }
-
-  function updateBanner() {
-    if (el) {
-      el.innerHTML = !accepted ? `
-        <div style="
-          position: fixed;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          background: #222;
-          color: #fff;
-          padding: 1.1em 2em;
-          z-index: 2147483647;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          font-size: 1.1em;
-          box-shadow: 0 -2px 12px #0006;
-          border-top: 3px solid #e93c2f;
-        ">
-          <span>
-            CIBUBBLE uses local storage for login and site preferences. By using the site, you agree to this.
-            <a href="/privacy" target="_blank" rel="noopener" style="color:#e93c2f;text-decoration:underline;margin-left:0.7em;font-size:1em;">Learn more</a>.
-          </span>
-          <button style="background:#e93c2f;color:#fff;border:none;padding:0.5em 1.5em;border-radius:9px;font-size:1.1em;font-weight:700;cursor:pointer;margin-left:1.7em;" onclick="document.getElementById('cookie-consent-root').svelteAcceptCookies()">OK</button>
-        </div>
-      ` : '';
-      // Make the accept function available to the raw HTML
-      el.svelteAcceptCookies = acceptCookies;
+      console.log('Accepted set, banner should vanish');
     }
   }
 </script>
 
-
 {#if !accepted}
-  <div class="cookie-banner">
-    <span>
-      CIBUBBLE uses local storage for login and site preferences. By using the site, you agree to this.
-      <a href="/privacy" target="_blank" rel="noopener">Learn more</a>.
-    </span>
-    <button on:click={acceptCookies}>OK</button>
+  <div id="cookie-banner-test" style="
+    position: fixed;
+    left: 20vw;
+    top: 20vh;
+    width: 60vw;
+    height: 30vh;
+    background: #e93c2f;
+    color: #fff;
+    z-index: 1000000;
+    font-size: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 5px solid yellow;
+    box-shadow: 0 0 30px #000;
+    text-align: center;
+    ">
+    <div>
+      🚨 BANNER TEST: If you see this, the CookieConsent component is rendering! 🚨
+    </div>
+    <button on:click={acceptCookies} style="font-size:1em;padding:1em 2em;margin-top:2em;">OK</button>
   </div>
 {/if}
-
-<style>
-.cookie-banner {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: #222;
-  color: #fff;
-  padding: 1.1em 2em;
-  z-index: 99999;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 1.1em;
-  box-shadow: 0 -2px 12px #0006;
-  border-top: 3px solid #e93c2f;
-}
-.cookie-banner button {
-  background: #e93c2f;
-  color: #fff;
-  border: none;
-  padding: 0.5em 1.5em;
-  border-radius: 9px;
-  font-size: 1.1em;
-  font-weight: 700;
-  cursor: pointer;
-  margin-left: 1.7em;
-}
-.cookie-banner a {
-  color: #e93c2f;
-  text-decoration: underline;
-  margin-left: 0.7em;
-  font-size: 1em;
-}
-@media (max-width: 600px) {
-  .cookie-banner {
-    flex-direction: column;
-    align-items: stretch;
-    font-size: 0.98em;
-    padding: 0.7em 0.7em;
-  }
-  .cookie-banner button {
-    margin-left: 0;
-    margin-top: 1em;
-  }
-}
-</style>
