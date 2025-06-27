590768104d1c236f6115e3f6f5e9e0ab3a24613d
Dave <dave@localhost.localdomain>
2025-06-27 11:09:01 +0100

    add style to cookie warning

--- src/lib/components/CookieConsent.svelte
+++ src/lib/components/CookieConsent.svelte
@@ -23,3 +23,39 @@
     <button on:click={acceptCookies}>OK</button>
   </div>
 {/if}
+
+<style>
+.cookie-banner {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #222 !important;
+  color: #fff;
+  padding: 1.5em 2em;
+  z-index: 99999 !important;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.2em;
+  box-shadow: 0 -2px 12px #0006;
+  border-top: 3px solid #e93c2f;
+}
+.cookie-banner button {
+  background: #e93c2f;
+  color: #fff;
+  border: none;
+  padding: 0.7em 2em;
+  border-radius: 9px;
+  font-size: 1.1em;
+  font-weight: 700;
+  cursor: pointer;
+  margin-left: 2em;
+}
+.cookie-banner a {
+  color: #e93c2f;
+  text-decoration: underline;
+  margin-left: 0.7em;
+  font-size: 1em;
+}
+</style>
